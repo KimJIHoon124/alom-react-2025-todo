e230b724c92eb83888b4814d63a8ceb067d0f5b3
KIMJIHOON <gse11024@naver.com>
2025-05-18 20:16:10 +0900

    aa

--- src/assignments/example.js
+++ src/assignments/example.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 function App() {
   // 과제1-1: 7-1, 7-2강을 듣고 이곳에 투두리스트 컴포넌트를 작성해주세요.
+  const [todo, setTodo] = useState("");
+  const onChange = (event) => setTodo(event.target.value);
+  console.log(todo);
 
   return (
     <div>
