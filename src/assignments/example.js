b966bd01ab67f7b6772319017d23f6452d1f5b87
KIMJIHOON <gse11024@naver.com>
2025-05-18 20:45:17 +0900

    최종 투두 완성

--- src/assignments/example.js
+++ src/assignments/example.js
@@ -1,16 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styles from "./todo-list.module.css";
+import TodoItem from "./todo-item";
 
-function App() {
-  // 과제1-1: 7-1, 7-2강을 듣고 이곳에 투두리스트 컴포넌트를 작성해주세요.
-  const [todo, setTodo] = useState("");
-  const onChange = (event) => setTodo(event.target.value);
-  console.log(todo);
+function Example() {
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function onChange(e) {
+    setTodo(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    if (!toDo.trim()) {
+      alert("올바른 값을 입력해주세요");
+      setTodo("");
+      return;
+    }
+    if (toDos.includes(toDo)) {
+      alert("이미 같은 값이 존재합니다");
+      return;
+    }
+    setTodos((prev) => [toDo, ...prev]);
+    setTodo("");
+  }
+
+  function onDelete(content) {
+    const newTodos = toDos.filter((todo) => todo !== content);
+    setTodos(newTodos);
+  }
+
+  useEffect(() => {
+    const saved = localStorage.getItem("my_todos");
+    if (saved) {
+      setTodos(JSON.parse(saved));
+    }
+    setLoading(true);
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      localStorage.setItem("my_todos", JSON.stringify(toDos));
+    }
+  }, [toDos, loading]);
 
   return (
-    <div>
-      <h1>My To Dos</h1>
+    <div className={styles.container}>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit} className={styles.form_container}>
+        <input
+          type="text"
+          value={toDo}
+          onChange={onChange}
+          placeholder="할 일을 입력하세요."
+        />
+        <button type="submit">작성하기</button>
+      </form>
+      <hr />
+      {toDos.length === 0 ? (
+        <div>예정된 할 일이 없습니다.</div>
+      ) : (
+        <div className={styles.todo_list_container}>
+          {toDos.map((todo, idx) => (
+            <TodoItem key={idx} content={todo} onDelete={onDelete} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default Example;
